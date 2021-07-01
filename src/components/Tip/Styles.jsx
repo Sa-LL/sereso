a4c7b1d4b6706f1dbe1096892616e04765bf2c32
Santiago <santiago.londono@utp.edu.co>
2021-07-01 12:10:40 -0500

    fixed views

--- src/components/Tip/Styles.jsx
+++ src/components/Tip/Styles.jsx
@@ -16,8 +16,8 @@ export const useStyles = makeStyles((theme) => ({
 		display: 'flex',
 		flexDirection: 'column',
 		justifyContent: 'center',
-		[theme.breakpoints.down('md')]: {
-			minHeight: '320px',
+		[theme.breakpoints.down('lg')]: {
+			minHeight: '350px',
 		},
 	},
 	tipTitle: {
@@ -25,18 +25,18 @@ export const useStyles = makeStyles((theme) => ({
 	},
 	img: {
 		height: '150px',
-		[theme.breakpoints.down('md')]: {
-			height: '100px',
+		[theme.breakpoints.down('lg')]: {
+			height: '110px',
 		},
 	},
 	title: {
-		[theme.breakpoints.down('md')]: {
+		[theme.breakpoints.down('lg')]: {
 			fontSize: '1.2rem',
 		},
 	},
 	details: {
-		[theme.breakpoints.down('md')]: {
-			fontSize: '0.750rem',
-		},
+		// [theme.breakpoints.down('lg')]: {
+		// 	fontSize: '0.750rem',
+		// },
 	},
 }));
